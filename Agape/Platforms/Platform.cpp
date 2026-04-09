514162ba8f08b9f55f8951bc371fca77aad81801
Lauren Glina <hello@glina.com.au>
2026-04-09 18:20:51 +1000

    Generate an after boot event so, e.g. settings can be loaded from flash post-init

--- Agape/Platforms/Platform.cpp
+++ Agape/Platforms/Platform.cpp
@@ -9,6 +9,13 @@ void Platform::registerListener( EventListener* listener )
     m_eventListeners.push_back( listener );
 }
 
+void Platform::doBootCompleted()
+{
+    Event event;
+    event.m_type = bootCompleted;
+    dispatchEvent( event );
+}
+
 void Platform::dispatchEvent( const Event& event )
 {
     for( Vector< EventListener* >::const_iterator it( m_eventListeners.begin() ); it != m_eventListeners.end(); ++it )
