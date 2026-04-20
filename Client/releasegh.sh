ec2bccf240cd6ae165398c39c169921c25c0d855
Lauren Glina <hello@glina.com.au>
2026-04-21 01:05:53 +1000

    Commit Version.cpp before tag and release

--- Client/releasegh.sh
+++ Client/releasegh.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-./version.py -inc && make rebuild -j && gh release create b`./version.py` agape.hex
+./version_tool.py -inc && \
+    make rebuild -j && \
+    git commit -m "Increment" Version.cpp && \
+    git push && \
+    gh release create b`./version_tool.py` --title "b`./version_tool.py`" agape.hex
